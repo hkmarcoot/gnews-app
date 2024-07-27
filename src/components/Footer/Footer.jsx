3acdb4f43f482ed3a365cd38010fd318a39f295f
hkmarcoot <hkmarcoot@gmail.com>
2024-07-27 20:26:23 +0100

    Add SOC logo image to images folder

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -10,11 +10,7 @@ export default function Footer() {
     <footer className="font-mono border-none h-full bg-gradient-to-b from-sky-100 to-blue-200">
       <div className="mx-16 py-16 md:flex md:flex-row md:justify-around md:mx-8">
         <div>
-          <img
-            src="https://cademy-images.b-cdn.net/efc9bc09-5362-4f9b-9d08-1586a423746f/580c3981-14aa-44b8-9c09-70f32af66aa1/original.png?width=256"
-            alt="School Of Code Logo"
-            width="50rem"
-          />
+          <img src="" alt="School Of Code Logo" width="50rem" />
           <p className="mt-4 text-gray-500">
             Simple News API to search<br></br> News and Blog Articles
           </p>
